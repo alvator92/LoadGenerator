68d5c883293691bf009ae2ab9c957a0b2dc115fa
alvator92 <alvator@list.ru>
2022-10-17 16:00:33 +0300

    add Configuration DataSource

--- src/main/java/ru/test/generator/StartApplication.java
+++ src/main/java/ru/test/generator/StartApplication.java
@@ -1,13 +1,16 @@
 package ru.test.generator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class StartApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(StartApplication.class, args);
+        System.out.println("Hello LoadGen!");
 
     }
 }
